1ba5dc1fdd906f768f55c53738870b16bab0bc05
Vlad Vanyushin <vvanyushin@kameleoon.com>
2019-10-18 12:51:39 +0300

    moved multimedia in index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const multimediaScript = document.createElement('script');
-    multimediaScript.setAttribute('src','./js/multimedia.js');
+    var multimedia = document.createElement('script');
+    multimedia.src = '//zen.yandex.ru/widget-loader';
+    document.head.appendChild(multimedia);
 
     const buttonsContainer = document.querySelector('.buttons-wrap');
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
         '<button class="button">Нет</button>';
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
+
 });
--- js/multimedia.js
+++
@@ -1,5 +0,0 @@
-(function(w, d, n) {
-    var s = d.createElement('script');
-    s.src = '//zen.yandex.ru/widget-loader';
-    d.head.appendChild(s);
-})(window, document, 'yandexZenAsyncCallbacks')
