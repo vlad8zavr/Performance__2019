a15627d4c5abfab30634e0eac5821296e33e6396
Vlad Vanyushin <vvanyushin@kameleoon.com>
2019-10-18 12:10:35 +0300

    back to previous

---
+++ js/multimedia.js
@@ -0,0 +1,5 @@
+(function(w, d, n) {
+    var s = d.createElement('script');
+    s.src = '//zen.yandex.ru/widget-loader';
+    d.head.appendChild(s);
+})(window, document, 'yandexZenAsyncCallbacks')
