f9338795fa1c0269cacd5bdf62ff6858b3666530
Vlad Vanyushin <vvanyushin@kameleoon.com>
2019-10-18 17:26:25 +0300

    main.js continue

--- js/main.js
+++ js/main.js
@@ -24521,66 +24521,6 @@ function getMouseAngle(event, centerElem) {
     return rad;
 }
 
-let knobDragged;
-let prevAngleRad = null;
-let prevRotate = null;
-
-function startDragging(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const rad = getMouseAngle(e, document.querySelector('.knob_center'));
-
-    knobDragged = true;
-    prevAngleRad = rad;
-    prevRotate = curRotate;
-}
-
-function stopDragging(e) {
-    knobDragged = false;
-}
-
-function dragRotate(e) {
-    if (!knobDragged) {
-        return;
-    }
-
-    const old = prevAngleRad;
-    let rad = getMouseAngle(e, document.querySelector('.knob_center'));
-    let delta = rad - old;
-
-    prevAngleRad = rad;
-
-    if (delta < 0) {
-        delta += Math.PI * 2;
-    }
-    if (delta > Math.PI) {
-        delta -= Math.PI * 2;
-    }
-
-    const deltaRotate = delta / Math.PI / 2;
-    const rotate = prevRotate + deltaRotate;
-
-    prevRotate = rotate;
-    setRotate(rotate);
-}
-
-function setEvtListeners() {
-    const elem = document.querySelector('.knob-container');
-
-}
-
-setEvtListeners();
-
-
-document.querySelectorAll('.modal_close').forEach(b => {
-    b.onclick = function() {
-        document.querySelectorAll('.modal').forEach(m => {
-            m.classList.toggle('modal_open', false);
-            document.querySelector('body').style.overflow = 'auto';
-        });
-    }
-});
-
 const TEMPS = {
     'manual': -10,
     'cold': 0,
@@ -24588,30 +24528,6 @@ const TEMPS = {
     'hot': 30
 }
 
-document.querySelectorAll('.modal__filter-item_temp').forEach(l => {
-    l.onclick = function() {
-        document.querySelector('.adjust-bar_theme_temp').value = TEMPS[this.id];
-        document.querySelector('.modal_temp .modal__value').innerHTML = TEMPS[this.id] > 0 ? '+' + TEMPS[this.id] : TEMPS[this.id];
-    }
-});
-
-const showModal = function(selector) {
-    document.querySelector(selector).classList.toggle('modal_open', true);
-    document.querySelector('body').style.overflow = 'hidden';
-}
-
-
-document.querySelectorAll('.panel_lamp').forEach(p => {
-    p.onclick = function() {
-        showModal('.modal_light');
-    }
-});
-
-document.querySelectorAll('.panel_floor').forEach(p => {
-    p.onclick = function() {
-        showModal('.modal_knob');
-    }
-});
 
 document.addEventListener("DOMContentLoaded", function () {
     $('.card').each(function(e) {
@@ -24651,7 +24567,6 @@ let currentPage = 1;
 
 const selectButton = document.querySelector('.filter__select-button');
 const selectButtonText = document.querySelector('.filter__select-button .button__text');
-const selectOptions = document.querySelectorAll('.filter__select-item');
 const popup = document.querySelector('.filter__select-popup');
 
 
@@ -24660,17 +24575,6 @@ let widths = '';
 window.addEventListener('scroll', function() {
     widths += document.querySelectorAll('body')[0].offsetWidth;
 
-});
-
-selectOptions.forEach(o => {
-    o.addEventListener('click', function(e) {
-        document.querySelector('#' + e.target.dataset.group).checked = true;
-
-        selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));
-        e.target.classList.toggle('filter__select-item_checked', true);
-        popup.classList.toggle('filter__select-popup_open', false);
-        selectButtonText.innerText = e.target.innerText;
-    })
 });
 
 $(document).ready(function(){
