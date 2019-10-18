a04a8d7d8fbf83facd4453d9b2703d394f47607b
Vlad Vanyushin <vvanyushin@kameleoon.com>
2019-10-18 16:40:10 +0300

    async index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-    // var multimedia = document.createElement('script');
-    // multimedia.src = '//zen.yandex.ru/widget-loader';
-    // document.head.appendChild(multimedia);
-
     const buttonsContainer = document.querySelector('.buttons-wrap');
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
         '<button class="button">Нет</button>';
