99205af636e010b2082c6814b682e4b558887fc8
Vlad Vanyushin <vvanyushin@kameleoon.com>
2019-10-18 18:41:18 +0300

    zafiksiroval last changes

--- js/index.js
+++
@@ -1,6 +0,0 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const buttonsContainer = document.querySelector('.buttons-wrap');
-//     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-//         '<button class="button">Нет</button>';
-
-// });
