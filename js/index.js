5b3af7e67ab9fc59836ea731679253d949dc509c
Vlad Vanyushin <vvanyushin@kameleoon.com>
2019-10-18 16:30:45 +0300

    multimedia in index async

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-    var multimedia = document.createElement('script');
-    multimedia.src = '//zen.yandex.ru/widget-loader';
-    document.head.appendChild(multimedia);
+    // var multimedia = document.createElement('script');
+    // multimedia.src = '//zen.yandex.ru/widget-loader';
+    // document.head.appendChild(multimedia);
 
     const buttonsContainer = document.querySelector('.buttons-wrap');
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
